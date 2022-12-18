6e489dff9ff33287d9c1ba3cb222cefdb1814b3e
James Calligeros <jcalligeros99@gmail.com>
2022-12-18 15:45:31 +1000

    ASoC: tas2770: copy working PM ops from tas2764
    The PM routine for the TAS2770 and TAS2764 codecs should be identical,
    however the PM ops for the TAS2770 code diverges significantly and is
    broken in more ways than TAS2764 was.
    
    The suspend function attempts to pull the codec into hardware shutdown
    before putting the power register into the software shutdown state,
    which it doesn't even attempt to do if the SDZ_GPIO pin is present.
    The same is true on resume, where it does not try to restore the
    codec into a known power state.
    
    Copy the known-good PM ops from tas2764.c.
    
    Signed-off-by: James Calligeros <jcalligeros99@gmail.com>

--- sound/soc/codecs/tas2770.c
+++ sound/soc/codecs/tas2770.c
@@ -72,25 +72,19 @@ static int tas2770_codec_suspend(struct snd_soc_component *component)
 	struct tas2770_priv *tas2770 = snd_soc_component_get_drvdata(component);
 	int ret = 0;
 
+	ret = snd_soc_component_update_bits(component, TAS2770_PWR_CTRL,
+					    TAS2770_PWR_CTRL_MASK,
+					    TAS2770_PWR_CTRL_SHUTDOWN);
+	if (ret < 0)
+		return ret;
+
+	if (tas2770->sdz_gpio)
+		gpiod_set_value_cansleep(tas2770->sdz_gpio, 0);
+
 	regcache_cache_only(tas2770->regmap, true);
 	regcache_mark_dirty(tas2770->regmap);
 
-	if (tas2770->sdz_gpio) {
-		gpiod_set_value_cansleep(tas2770->sdz_gpio, 0);
-	} else {
-		ret = snd_soc_component_update_bits(component, TAS2770_PWR_CTRL,
-						    TAS2770_PWR_CTRL_MASK,
-						    TAS2770_PWR_CTRL_SHUTDOWN);
-		if (ret < 0) {
-			regcache_cache_only(tas2770->regmap, false);
-			regcache_sync(tas2770->regmap);
-			return ret;
-		}
-
-		ret = 0;
-	}
-
-	return ret;
+	return 0;
 }
 
 static int tas2770_codec_resume(struct snd_soc_component *component)
@@ -101,15 +95,19 @@ static int tas2770_codec_resume(struct snd_soc_component *component)
 	if (tas2770->sdz_gpio) {
 		gpiod_set_value_cansleep(tas2770->sdz_gpio, 1);
 		usleep_range(1000, 2000);
-	} else {
-		ret = tas2770_update_pwr_ctrl(tas2770);
-		if (ret < 0)
-			return ret;
 	}
 
 	regcache_cache_only(tas2770->regmap, false);
 
-	return regcache_sync(tas2770->regmap);
+	ret = regcache_sync(tas2770->regmap);
+	if (ret < 0)
+		return ret;
+
+	ret = tas2770_update_pwr_ctrl(tas2770);
+	if (ret < 0)
+		return ret;
+
+	return 0;
 }
 #else
 #define tas2770_codec_suspend NULL
