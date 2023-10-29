6a5fc22fc072d686ec0fffaad4b38856b9c279ec
Hector Martin <marcan@marcan.st>
2023-10-29 22:02:30 +0900

    ASoC: tas2770: Support setting the PDM TX slot
    We don't actually support configuring the PDM input right now. Rather,
    this is useful as a hack.
    
    On Apple Silicon machines, amps are split between two I2S buses which
    are logically ANDed internally at the SoC. Odd and even slot groups are
    driven by amps on either bus respectively. Since the signals are ANDed,
    unused slot groups must be driven as zero to avoid corrupting the data
    from the other side.
    
    On most recent machines (TAS2764-based), this is accomplished using the
    "SDOUT zero mask" feature of that chip. Unfortunately, TAS2770 does not
    support this. It does support zeroing out *all* unused slots, which
    works well for machines with a single amp per I2S bus. That is all,
    except one.
    
    The 13" M1 MacBook Pro is the only machine using TAS2770 and two amps
    per I2S bus:
    
    L Bus: SPK0I SPK0V Hi-Z  Hi-Z  SPK2I SPK2V Hi-Z  Hi-Z
    R Bus: Hi-Z  Hi-Z  SPK1I SPK2V Hi-Z  Hi-Z  SPK3I SPK3V
    
    To ensure uncorrupted data, we need to force all the Hi-Z periods to
    zero. We cannot use the "force all zero" feature, as that would cause a
    bus conflict between both amps. We can use the pull-down feature, but
    that leaves a few bits of garbage on the trailing edge of the speaker
    data, since the pull-down is weak.
    
    This is where the PDM transmit feature comes in. With PDM grounded and
    disabled (the default state), the PDM slot is transmitted as all zeroes.
    We can use that to force a zero 16-bit slot after the voltage data for
    each speaker, cleaning it up. Then the pull-down ensures the line stays
    low for the subsequent slot:
    
    L Bus: SPK0I SPK0V PDM0  PulDn SPK2I SPK2V PDM0  PulDn
    R Bus: PDM0  PulDn SPK1I SPK2V PDM0  PulDn SPK3I SPK3V
    
    Yes, this is a horrible hack, but it beats adding dummy slots that would
    be visible to the userspace capture side. There may be some other way to
    fix the logical AND behavior on the MCA side... that would make this
    unnecessary.
    
    ("How does Apple deal with this"? - they don't, macOS does not use
    IVSENSE on TAS2770 machines even though it's physically wired up,
    but we want to do so on Linux.)
    
    Reviewed-by: Neal Gompa <neal@gompa.dev>
    Signed-off-by: Hector Martin <marcan@marcan.st>
    Signed-off-by: James Calligeros <jcalligeros99@gmail.com>

--- sound/soc/codecs/tas2770.c
+++ sound/soc/codecs/tas2770.c
@@ -241,6 +241,19 @@ static int tas2770_set_ivsense_transmit(struct tas2770_priv *tas2770,
 	return 0;
 }
 
+static int tas2770_set_pdm_transmit(struct tas2770_priv *tas2770, int slot)
+{
+	struct snd_soc_component *component = tas2770->component;
+	int ret;
+
+	ret = snd_soc_component_update_bits(component, TAS2770_TDM_CFG_REG7,
+					    TAS2770_TDM_CFG_REG7_PDM_MASK |
+					    TAS2770_TDM_CFG_REG7_50_MASK,
+					    TAS2770_TDM_CFG_REG7_PDM_ENABLE |
+					    slot);
+	return ret;
+}
+
 static int tas2770_set_bitwidth(struct tas2770_priv *tas2770, int bitwidth)
 {
 	int ret;
@@ -626,6 +639,13 @@ static int tas2770_codec_probe(struct snd_soc_component *component)
 			return ret;
 	}
 
+	if (tas2770->pdm_slot != -1) {
+		ret = tas2770_set_pdm_transmit(tas2770, tas2770->pdm_slot);
+
+		if (ret < 0)
+			return ret;
+	}
+
 	return 0;
 }
 
@@ -764,6 +784,11 @@ static int tas2770_parse_dt(struct device *dev, struct tas2770_priv *tas2770)
 
 		tas2770->v_sense_slot = -1;
 	}
+
+	rc = fwnode_property_read_u32(dev->fwnode, "ti,pdm-slot-no",
+				      &tas2770->pdm_slot);
+	if (rc)
+		tas2770->pdm_slot = -1;
 
 	tas2770->sdz_gpio = devm_gpiod_get_optional(dev, "shutdown", GPIOD_OUT_HIGH);
 	if (IS_ERR(tas2770->sdz_gpio)) {
