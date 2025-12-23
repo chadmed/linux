214560617d94f4b82551ed6209820f95c192253c
James Calligeros <jcalligeros99@gmail.com>
2025-12-23 19:38:12 +1000

    drm: apple: use the zpos property directly for surface ordering
    We don't need to skip the bottommost surface anymore, so just use
    zpos as the index into the swap request.
    
    Signed-off-by: James Calligeros <jcalligeros99@gmail.com>

--- drivers/gpu/drm/apple/iomfb_template.c
+++ drivers/gpu/drm/apple/iomfb_template.c
@@ -1319,7 +1319,7 @@ void DCP_FW_NAME(iomfb_flush)(struct apple_dcp *dcp, struct drm_crtc *crtc, stru
 		 * also unusable on some machines, so ignore it.
 		 */
 
-		l = MAX_BLEND_SURFACES - new_state->normalized_zpos;
+		l = new_state->normalized_zpos;
 
 		WARN_ON(l > MAX_BLEND_SURFACES);
 
